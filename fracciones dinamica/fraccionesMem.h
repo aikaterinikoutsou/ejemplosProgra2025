78060fb383fd552193454f5d5118ce037d115f3e
aikaterinikoutsou <aikaterini.koutsou@ceu.es>
2025-05-22 14:55:04 +0200

    fracciones dinamica

---
+++ fracciones dinamica/fraccionesMem.h
@@ -0,0 +1,16 @@
+#ifndef FRACCIONESMEM
+#define FRACCIONESMEM
+
+#include "fracciones.h"
+
+#define numFr 10
+
+Fraccion*  fracciones;
+
+extern void initializeFraccMem();
+extern Fraccion leerFraccion();
+extern void showFracciones();
+extern void printFraccion(Fraccion fr);
+extern void chooseFracciones(int* ind1, int* ind2);
+
+#endif
---
+++ fracciones dinamica/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "fracciones.h"
+#include "fraccionesMem.h"
+
+
+int main()
+{
+
+    int opcion = 0;
+    int salir = 0;
+    Fraccion fr1, fr2;
+    int ind_fr1=0, ind_fr2=0;
+
+    initializeFraccMem();
+
+    while(!salir)
+    {
+        opcion=printMenu();
+        switch(opcion) {
+
+            case 1:    //addFraccion
+                fr1 = leerFraccion();
+                addFraccion(fr1);
+                break;
+
+            case 2: //mostrarfracciones
+                showFracciones();
+                break;
+            case 3: //multiplicarFracciones
+                chooseFracciones(&ind_fr1, &ind_fr2);
+                printFraccion(multiplicarFracciones(*(fracciones+ind_fr1), *(fracciones+ind_fr2)));
+                break;
+            case 4: //salir
+                    salir=1;
+                    break;
+            default:
+                    printf("\nOpcion NO valida vuelve a elegir.\n");
+
+        }
+    }
+
+    return 0;
+}
+
+int printMenu()
+{
+    int opcion=0;
+
+    printf("Elija una opcion:\n");
+    printf("1) Introducir Fraccion\n");
+    printf("2) Mostrar Fracciones\n");
+    printf("3) Multiplicar Fracciones\n");
+    printf("4) Salir");
+
+    scanf("%d", &opcion);
+    return opcion;
+}
